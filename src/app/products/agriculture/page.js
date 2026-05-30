269c93ad0450df9f9cc2d485d891da33b914af1f
Rosen Stanev <rossen.t.stanev@gmail.com>
2026-05-30 10:29:49 +0300

    feat: add temporary noindex to category placeholders

--- src/app/products/agriculture/page.js
+++ src/app/products/agriculture/page.js
@@ -1,5 +1,12 @@
 import CategoryPlaceholderPage from '../components/CategoryPlaceholderPage.js';
 
+export const metadata = {
+  robots: {
+    index: false,
+    follow: true,
+  },
+};
+
 export default function AgriculturePage() {
   return <CategoryPlaceholderPage categoryId="agriculture" />;
 }
--- src/app/products/food-industry-equipment/page.js
+++ src/app/products/food-industry-equipment/page.js
@@ -1,5 +1,12 @@
 import CategoryPlaceholderPage from '../components/CategoryPlaceholderPage.js';
 
+export const metadata = {
+  robots: {
+    index: false,
+    follow: true,
+  },
+};
+
 export default function FoodIndustryEquipmentPage() {
   return <CategoryPlaceholderPage categoryId="food-industry-equipment" />;
 }
